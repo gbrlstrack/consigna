b606477454e18dbee1627e3abbb347c43a1a9858
afelipepf7221 <afelipepf@gmail.com>
2025-11-15 01:30:41 -0300

    tentando configura o heroku

--- src/main/java/com/consigna/consigna/repository/UsuarioRepository.java
+++ src/main/java/com/consigna/consigna/repository/UsuarioRepository.java
@@ -2,9 +2,11 @@ package com.consigna.consigna.repository;
 
 import com.consigna.consigna.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByLogin(String login);
     Optional<Usuario> findByResetPasswordToken(String token);
